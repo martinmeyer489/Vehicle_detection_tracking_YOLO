5775f2db268e8de960be5732426bb490a7200865
Chris11tian <christian24bryant@gmail.com>
2020-11-26 16:46:48 +0100

    läuft

--- prototypes/Christian/Livestream.py
+++ prototypes/Christian/Livestream.py
@@ -18,7 +18,7 @@ URLHD = "https://media.dcaiti.tu-berlin.de/tccams/1c/axis-cgi/mjpg/video.cgi?cam
 RTSP_URL = URLHD
 
 # kernel for image dilation
-kernel = np.ones((10, 25), np.uint8)
+kernel = np.ones((8, 20), np.uint8)
 
 # font style
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -36,6 +36,13 @@ def main():
     firstImage = True
     firstContour = True
     cars = []
+    carsPerLane = [0, 0, 0, 0, 0, 0]
+    rWhiteA1 = False
+    rWhiteA2 = False
+    rWhiteA3 = False
+    lWhiteA1 = False
+    lWhiteA2 = False
+    lWhiteA3 = False
 
     while True:
         start = time.time()
@@ -44,45 +51,87 @@ def main():
         # read the next frame from stream
         (grabbed, frame) = vs.read()
 
+        mask = np.zeros_like(frame)
+        vertices = np.array([[0, 203], [1280, 203], [1280, 203], [0, 203],
+                             [0, 235], [1280, 235], [1280, 235], [0, 235],
+                             [0, 265], [1280, 265], [1280, 265], [0, 265]], np.int32)
+        # fill the mask
+        cv2.fillPoly(mask, [vertices], (255, 255, 255))
+        # show ROI only
+        masked_frame = cv2.bitwise_and(frame, mask)
+
         if not grabbed:
             print('[ERROR] unable to grab input - check connection or link')
             break
         # track_cars(frame, str(fps._numFrames))
 
-        grayB = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        grayB = cv2.cvtColor(masked_frame, cv2.COLOR_BGR2GRAY)
 
         if not firstImage:
             diff_image = cv2.absdiff(grayB, grayA)
 
             # image thresholding
-            ret, thresh = cv2.threshold(diff_image, 25, 255, cv2.THRESH_BINARY)
+            ret, thresh = cv2.threshold(diff_image, 20, 255, cv2.THRESH_BINARY)
 
             # image dilation
             dilated = cv2.dilate(thresh, kernel, iterations=1)
+
+            # form eclipse shapes
             #E = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(20,20))
             #dst = cv2.morphologyEx(thresh, cv2.MORPH_GRADIENT, E)
 
             # find contours
             contoursB, hierarchy = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-            # if not firstContour:
-            #     for i, conB in enumerate(contoursB):
-            #         for conA in contoursA:
-            #             sameCon[i] = np.min(conB.mean - conA.mean)
-            #
-            # firstContour = False
-            # contoursA = contoursB
+
+            # fill contours in original frame
+            cv2.fillPoly(frame,contoursB,(255,255,255))
 
             # add contours to original frames
-            cv2.drawContours(frame, contoursB, -1, (127, 200, 0), 2)
+            #cv2.drawContours(frame, contoursB, -1, (127, 200, 0), 2)
 
 
-        # cv2.putText(frame, "vehicles detected: " + str(len(cars)), (55, 115), font, 0.6, (0, 180, 0), 2)
+            # find valid contours
+            rWhiteB1 = np.array_equal(frame[200, 1250], np.array([255,255,255],None))
+            if rWhiteB1 and not rWhiteA1:
+                carsPerLane[0] += 1
+            rWhiteA1 = rWhiteB1
+
+            rWhiteB2 = np.array_equal(frame[232, 1250], np.array([255, 255, 255], None))
+            if rWhiteB2 and not rWhiteA2:
+                carsPerLane[1] += 1
+            rWhiteA2 = rWhiteB2
+
+            rWhiteB3 = np.array_equal(frame[262, 1250], np.array([255, 255, 255], None))
+            if rWhiteB3 and not rWhiteA3:
+                carsPerLane[2] += 1
+            rWhiteA3 = rWhiteB3
+
+            
+            lWhiteB1 = np.array_equal(frame[200, 20], np.array([255,255,255],None))
+            if lWhiteB1 and not lWhiteA1:
+                carsPerLane[0] -= 1
+            lWhiteA1 = lWhiteB1
+
+            lWhiteB2 = np.array_equal(frame[232, 20], np.array([255, 255, 255], None))
+            if lWhiteB2 and not lWhiteA2:
+                carsPerLane[1] -= 1
+            lWhiteA2 = lWhiteB2
+
+            lWhiteB3 = np.array_equal(frame[262, 20], np.array([255, 255, 255], None))
+            if lWhiteB3 and not lWhiteA3:
+                carsPerLane[2] -= 1
+            lWhiteA3 = lWhiteB3
+
+
+
+        for i in range(len(carsPerLane)):
+            cv2.putText(frame, "Vehicles detected in lane " + str(i+1) + ": " + str(carsPerLane[i]), (55, 115+i*30), font, 0.6, (0, 180, 0), 2)
 
         if not firstImage:
-            cv2.imshow("Frame", cv2.resize(dilated, (800, 600)))
+             cv2.imshow("Frame", cv2.resize(dilated, (800, 600)))
 
-        #cv2.imshow("Frame", cv2.resize(frame, (800, 600)))
+        cv2.imshow("Frame", cv2.resize(frame, (800, 600)))
 
         grayA = grayB
         firstImage = False
